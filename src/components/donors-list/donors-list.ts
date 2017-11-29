80f23c83323920cca70b5232ecce46ec2e14a631
XuanPhuoc <wingame129@gmail.com>
2017-11-29 23:20:23 +0700

    @Phuoc has done banner card, waiting for review

--- src/components/donors-list/donors-list.ts
+++ src/components/donors-list/donors-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Input } from '@angular/core';
 import { AppControllerProvider } from '../../providers/football/app-controller/app-controller';
 import { Donors, DonorsList } from '../../providers/football/interfaces/fb-donors';
 import { Observable } from 'rxjs/Observable';
@@ -14,15 +14,22 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: 'donors-list.html'
 })
 export class DonorsListComponent {
+
+  @Input() background: string = "";
   listDonors : Observable<DonorsList>;
   
   isLoadingData : boolean = true;
+
+  isHaveBackround :boolean = false;
   constructor(private appController: AppControllerProvider) {
   }
   ngAfterViewInit(){
     this.listDonors = this.appController.getDornosList();
     this.listDonors.subscribe(donors=>{
       console.log(this.listDonors);
+      if(parseInt(this.background)==1){
+        this.isHaveBackround = true;
+      }
       this.isLoadingData = false;
     })
   }
